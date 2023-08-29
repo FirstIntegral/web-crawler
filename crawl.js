80149281ee1d738a56a83c25999bf2b255552822
brwsk <brwsk@protonmail.com>
2023-08-29 21:59:33 +0300

    recursively crawling pages

--- crawl.js
+++ crawl.js
@@ -1,26 +1,50 @@
 const { JSDOM } = require('jsdom')
 
-async function crawlPage(currentURL) {
+// 3 arguments. baseURL for the website we wanna crawl. currentURL for the current page. pages to keep track of the pages we've crawled so far
+async function crawlPage(baseURL, currentURL, pages) {
+
+    // Making sure the currentURL is based on the baseURL, you don't wanna crawl another website if there's an external website in the page linking to another website
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
     console.log(`actively crawling: ${currentURL}`)
 
     try {
         const response = await fetch(currentURL)
         if (response.status > 399) {
             console.log(`error in fetch with status code: ${response.status} on page: ${currentURL}`)
-            return
+            return pages
         }
 
         // After getting a response back, need to check if it's actually html before going any further 
         const content = response.headers.get("content-type")
         if (!content.includes("text/html")) {
             console.log(`non html response, content type: ${content}, on page: ${currentURL}`)
-            return
+            return pages
         }
 
-        console.log(await response.text())
+        const htmlBody = await response.text()
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+
+        for (nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+
+
     } catch (err) {
         console.log(`error in fetch: ${err.message}, on page: ${currentURL}`)
     }
+    return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
